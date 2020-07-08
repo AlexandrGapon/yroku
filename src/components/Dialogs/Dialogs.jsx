13fa16510c8934963e35a9ca34205442dc42d01f
unknown <gapon769@gmail.com>
2020-07-09 02:09:30 +0300

    dialoGGs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -3,8 +3,17 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return(
-        <div>
-            Dialogs
+        <div className = {s.dialogs}>
+            <div className = {s.dialogsItems}>
+                <div className = {s.dialog + ' ' + s.active}>
+                    123
+                </div>
+            </div>
+            <div className = {s.messages}>
+                <div className = {s.message}>
+                    1234
+                </div>
+            </div>
         </div>
     )
 }
